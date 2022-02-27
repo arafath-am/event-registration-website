780255a4f0b8c99afe0c0ed15d80de3a620ff4b5
arafath-am <arafathamer.m@gmail.com>
2022-02-26 19:08:47 -0600

    update color scheme

---
+++ src/main/java/com/example/eventregistration/dto/UserRegistrationDto.java
@@ -0,0 +1,24 @@
+package com.example.eventregistration.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationDto {
+
+    @NotBlank(message = "Full name is required")
+    @Size(max = 100, message = "Name must be less than 100 characters")
+    private String fullName;
+
+    @Email(message = "Enter a valid email")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
